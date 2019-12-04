0855eb29909f17106d9887c9977e20d9396ba8da
EUGINELETHAL <ochungeugine@gmail.com>
2019-12-04 15:48:36 +0300

    Updates

---
+++ precache-manifest.a6aacd58b18746ce2c1ef87ddd29460f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04cc5c4392d000cadd83bcaeb9ef4aa9",
+    "url": "/Miniflix-v2/index.html"
+  },
+  {
+    "revision": "12e3e5132f7eaf5e93d5",
+    "url": "/Miniflix-v2/static/css/main.99568568.chunk.css"
+  },
+  {
+    "revision": "9839789da04e7998a6c9",
+    "url": "/Miniflix-v2/static/js/2.d4ae6b7f.chunk.js"
+  },
+  {
+    "revision": "12e3e5132f7eaf5e93d5",
+    "url": "/Miniflix-v2/static/js/main.3242a6c1.chunk.js"
+  },
+  {
+    "revision": "cbc29be8be7878f41cc5",
+    "url": "/Miniflix-v2/static/js/runtime~main.05063524.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,11 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-<<<<<<< HEAD
-  "/Miniflix/precache-manifest.d68921d86c0fe5e93c67d05f6ea91c72.js"
-=======
-  "/Miniflix/precache-manifest.4993888055039d7284ba686933bc2802.js"
->>>>>>> 1fcb19f2da42219e2559567d2bc65d2392ee7209
+  "/Miniflix-v2/precache-manifest.a6aacd58b18746ce2c1ef87ddd29460f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -37,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Miniflix/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Miniflix-v2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
